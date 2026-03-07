899c0170582c46e95a88cfa5d7f2d060467abbdf
Xpartiel <xparting@ciencias.unam.mx>
2026-03-06 18:24:08 -0600

    refactor: Clase encapsulada y deslindada de main

--- P2/ColaConcurrente2.java
+++ P2/ColaConcurrente2.java
@@ -16,75 +16,117 @@ import java.util.concurrent.locks.ReentrantLock;
  * y tampoco Syncronized.
  * 
  */
-public class ColaConcurrente2 {
+public class ColaConcurrente2 extends ColaSecuencial {
 
-    public static void main(String[] args)  throws InterruptedException, ExecutionException{
+    /**
+     * {@link List}a de {@link Future}s que seran usados para esperar el fin de la ejecucion concurrente
+     * para reportar sus resultados de tipo {@link String}
+     */
+    private List<Future<String>> futures;
+    /** @return {@link List}a que espera resultados de los hilos en ejecucion */
+    public List<Future<String>> getFutures() {
+        return futures;
+    }
+
+
+    /**
+     * {@link @ExecutorService} pool de hilos previamente apartados para su uso concurrente. */
+    private ExecutorService threadPool;
+    /** @return {@link ExecutorService} los hilos en espera de ejecucion */
+    public ExecutorService getPool() {
+        return threadPool;
+    }
+
+
+    /**
+     * {@link Lock} de tipo {@link ReentrantLock} con que se aplica candado bloqueante
+     * a la funcionalidad de la {@link ColaConcurrente2}
+     */
+    private Lock lock;
+    /** @return {@link ReentrantLock} el candado que da turnos a los hilos para operar */
+    public Lock getLock() {
+        return lock;
+    }
+
+    /** Constructor por defecto, crea 4 hilos en espera */
+    public ColaConcurrente2(){
+        this( 4 );
+    }
+    /** 
+     * Constructor con argumento {@code int} correspondiente a la cantidad de hilos
+     * Se asegura que tenga cuanto menos un hilo asignado
+     */
+    public ColaConcurrente2( int threads ){
+        this.lock = new ReentrantLock();
+        this.futures = new ArrayList<>();
+        this.threadPool = (threads<2)?
+            Executors.newSingleThreadExecutor() :
+            Executors.newFixedThreadPool(threads) ;
+    }
+
+    public static void main(String[] args){
         
-        //Declaramos la cola que utilizaran los hilos.
-        ColaSecuencial cola= new ColaSecuencial();
-     
-        //Declaramos nuestro candado.
-        Lock lock = new ReentrantLock();
-
-        //Lista para poder guardar los resultados de los submit.
-        List<Future<String>> futures = new ArrayList<Future<String>>();
-
-        //Necesitamos declarar la pool de hilos (en este caso sera de 4 hilos).
-        ExecutorService pool = Executors.newFixedThreadPool(4);
+        //Declaramos la cola que utilizara los hilos.
+        ColaConcurrente2 cola = new ColaConcurrente2();
+        int espera = 1000;
+        int elementos = 1000;
 
         //Realizaremos un bucle for para anexar algunos items a la cola.
-        for (int i = 0; i <= 1000; i++){
-            
-                String val=Integer.toString(i);
-                //en esta parte los hilos intenetaran encolar al mismo tiempo.
-                pool.submit(()->{ lock.lock();
-                    try{cola.enq(val);
-                        
-                    }finally{
-                        lock.unlock();
-                    } });
+        for (int i = 0; i <= elementos; i++){
+            String val=Integer.toString(i);
+            //en esta parte los hilos intenetaran encolar al mismo tiempo.
+            cola.getPool().submit(()->{
+                cola.getLock().lock();
+                try{
+                    cola.enq(val);
+                }finally{
+                    cola.getLock().unlock();
+                }
+            });
         }
 
         try{
-			Thread.sleep(1000);// Delay para esperar que todas las tareas terminen
+			Thread.sleep(espera);// Delay para esperar que todas las tareas terminen
 		}catch(InterruptedException e) {
 			System.out.println(e);
 		}
 
         //Eliminamos los 1000 elementos
-        for (int i = 0; i <= 1000; i++){
-
-               Future<String> future = pool.submit(()->{ lock.lock();
-                    try{return cola.deq();
-                        
-                    }finally{
-                        lock.unlock();
-                    } });
-                futures.add(future);
+        for (int i = 0; i <= elementos; i++){
+            Future<String> future = cola.getPool().submit( () -> {
+                cola.getLock().lock();
+                try{
+                    return cola.deq();
+                }finally{
+                    cola.getLock().unlock();
+                }
+            });
+            cola.getFutures().add(future);
         }
 
         //Hacemos shutdown a la pool pues ya no la ocuparemos.
-        pool.shutdown();
+        cola.getPool().shutdown();
 
         //Utilizaremos Future para saber si se hicieron varios deq al mismo elemento.
         try{
             //Utiliaremos un bucle para recorrer la lista de futuros		
-			for (int i = 0; i < futures.size(); i++) {
+			for (int i = 0; i < cola.getFutures().size(); i++) {
                 //Si la tarea aun no ha terminado obligaremos a esperar a que termine.
-	            while(!futures.get(i).isDone());
+	            while(! cola.getFutures().get(i).isDone());
                 //Obtenemos el elemento que 'elimino' de la cola.
-	            String result = futures.get(i).get();
+	            String result = cola.getFutures().get(i).get();
 	            System.out.println("Result: "+result);
             }
 		}catch(InterruptedException e) {
 			System.out.println(e);
-		}
+		}catch(ExecutionException e){
+            System.out.println(e);
+        }
         
         try{
-			Thread.sleep(1000);// Delay para esperar que todas las tareas terminen
+			Thread.sleep(espera);// Delay para esperar que todas las tareas terminen
 		}catch(InterruptedException e) {
 			System.out.println(e);
 		}
-       
-    }    
+    }
 }
