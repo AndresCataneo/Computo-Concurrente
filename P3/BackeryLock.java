fcd0eff9d49f038fadd02341b62771f390738db9
Xpartiel <xparting@ciencias.unam.mx>
2026-03-14 13:34:45 -0600

    feat: Backery implementation now has stable thread IDs

--- P3/BackeryLock.java
+++ P3/BackeryLock.java
@@ -24,28 +24,45 @@ public class BackeryLock{
         return res;
     }
 
-    private boolean evaluaTupla( int labelK , int k , int labelI , int i ){
+    /**
+     * Necesitamos que cada hilo tenga un id unico, para ello declararemos
+     * una variable local para cada hilo. 
+    */
+    private static int idCounter = 0;
+    public static final ThreadLocal<Integer> threadId = ThreadLocal.withInitial(() -> {
+        synchronized ( BackeryLock.class ) { //Magia
+            return idCounter++;
+        }
+    });
+
+    /**
+     * Metodo auxiliar con que 
+     * @param k
+     * @param i
+     * @return
+     */
+    private boolean evaluaTupla( int k , int i ){
         if( label[k] == label[i] ){
-            if( k > i ){
+            if( i < k ){
                 return false;
             }
-        } else if( label[k] > label[i] ){
+        } else if( label[i] < label[k] ){
             return false;
         }
         return true;
     }
 
     public void lock(){
-
-        int i = (int)Thread.currentThread().getId()-35;
+        System.out.println("BEGIN LOCK");
+        int i = threadId.get();
         System.out.println("Este hilo tiene la ID "+i);
 
-        flag[i] = true;
-        // doorway = Maximo + 1
-        label[i] = maximo(label)+1;
-        for (int k=0; k<label.length; k++) {
-            System.out.println("El hilo a comparar tiene la ID "+k);
-            while( k != i &&  flag[k] && evaluaTupla( label[k] , k , label[i] , i) ){ /* No hace Nada */ }
+        flag[i] = true; // Este hilo pide permiso de pasar
+        
+        label[i] = maximo(label)+1; // Este hilo toma boleto (max+1)
+        for (int k=0; k<label.length; k++) { // Este hilo se compara al resto
+            System.out.println("El hilo con que "+i+" se compara tiene ID "+k);
+            while( k != i &&  flag[k] && evaluaTupla( k , i ) ){ /* No hace Nada */ }
         }
     }
 
@@ -58,6 +75,7 @@ public class BackeryLock{
     }
 
     public void unlock(){
-        flag[(int)Thread.currentThread().getId()-35] = false;
+        System.out.println("UNLOCK");
+        flag[threadId.get()] = false;
     }
 }
